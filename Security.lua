194635a8f3bad6c240582b98d4d47b1eb7fc7706
Vxspot <matasakurasemotetoxicoo@gmail.com>
2024-11-28 22:58:40 -0400

    Create Security.lua

---
+++ Security.lua
@@ -0,0 +1,20 @@
+local isRoblox = pcall(function()
+    
+    return game and game:IsA("DataModel")
+end)
+
+if not isRoblox then
+    
+    error("Error: No tienes permiso para ver este contenido.")
+    return
+end
+
+
+local file = io.open("VP_FAMILLY-LITLLE.bak", "r")
+if file then
+    local content = file:read("*a")
+    file:close()
+    load(content)()
+else
+    error("Error: No se pudo abrir el archivo VP_FAMILLY-LITLLE.")
+end
